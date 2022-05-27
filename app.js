1fa6a824337a8fd7f7da85376de253cec8b54bd8
Bakumenko <tinyy11@gmail.com>
2022-05-27 11:20:07 +0300

    add userSchema

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const NotFoundError = require('./errors/notFoundError');
+
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Маршрут не найден'));
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
