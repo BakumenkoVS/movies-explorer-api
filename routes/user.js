362a215614497a9c85a7ec2d5cdf0ad2a2bc6bd7
BakumenkoVS <tinyyy11@gmail.com>
2022-06-04 10:16:48 +0300

    bug fixes updateProfile

--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,7 @@ router.patch(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
+      email: Joi.string().email().required(),
     }),
   }),
   updateProfile,
